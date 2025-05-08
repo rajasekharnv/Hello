38d861da6fe101079f3af6a340f47bf8f8d2ed65
rajasekharnv <nv.rajasekhar@gmail.com>
2025-05-08 14:34:37 +0530

    added comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{
     public static void main (String args[]){
+        // Prints "Hello World" in the terminal window.
         System.out.println("Hello World from jenkins");
     }
 }
